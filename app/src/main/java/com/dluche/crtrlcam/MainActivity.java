8746a9b2805ced0943f73e04d412daf84a221484
DanielLuche <d.luche@namoadigital.com>
2018-12-04 14:57:27 -0200

    Testando Fabric.io para captura de exception não tratada
    https://get.fabric.io/
    https://fabric.io/kits/android/crashlytics

--- app/src/main/java/com/dluche/crtrlcam/MainActivity.java
+++ app/src/main/java/com/dluche/crtrlcam/MainActivity.java
@@ -1,22 +1,33 @@
 package com.dluche.crtrlcam;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
+import com.crashlytics.android.Crashlytics;
 import com.dluche.crtrlcam.ctrl.CtrlCamera;
+
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     private CtrlCamera ctrlCamera;
     private CtrlCamera ctrlCamera2;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //FABRIC
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
-        ctrlCamera = findViewById(R.id.main_ctrl_cam);
+        //btn.setText("Ronaldo");
+
+        //ctrlCamera = findViewById(R.id.main_ctrl_cam);
+        ctrlCamera = //findViewById(R.id.main_ctrl_cam);
         ctrlCamera2 = findViewById(R.id.main_ctrl_cam2);
         //
+        //ctrlCamera.setOnClickListener(null);
     }
 }
